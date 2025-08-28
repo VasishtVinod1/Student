0402e4ad2e219463a0a67dc05c11922fbc542ba0
vasisht.vd <vasisht.vd@PIC4316>
2025-08-28 10:52:06 +0530

    ticket -5

---
+++ Student.Services/Implementation/StudentRepository.cs
@@ -0,0 +1,31 @@
+﻿using SMS.Services.Interfaces;
+using SMS.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SMS.Services.Implementation
+{
+    public class StudentRepository : IStudentRepository;
+    {
+        private readonly AppDbContext _context;
+
+    public StudentRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+
+}
+}
+
+
+
+
+
+
+
+
