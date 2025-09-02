de8bec7b2693682929e55d4a642bc8b9e4a2e450
vasisht.vd <vasisht.vd@PIC4316>
2025-09-02 10:09:42 +0530

    Email Validation

--- Student.Services/DTO/StudentCreateDto.cs
+++ Student.Services/DTO/StudentCreateDto.cs
@@ -1,9 +1,12 @@
-﻿public class StudentCreateDto
+﻿using System.ComponentModel.DataAnnotations;
+
+public class StudentCreateDto
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; }
     public string Gender { get; set; } = string.Empty;
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
     public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
--- Student.Services/DTO/StudentUpdatedto.cs
+++ Student.Services/DTO/StudentUpdatedto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace SMS.Services.DTO
         public string LastName { get; set; } = string.Empty;
         public DateTime? DateOfBirth { get; set; }
         public string? Gender { get; set; }
+        [EmailAddress]
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
